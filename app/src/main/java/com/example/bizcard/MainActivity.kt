272e90829b4893c3c2a16f20d0cdb78db23fb571
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-03-21 23:37:51 +0100

    create profileImage composable, add column and a divider

--- app/src/main/java/com/example/bizcard/MainActivity.kt
+++ app/src/main/java/com/example/bizcard/MainActivity.kt
@@ -4,20 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.bizcard.ui.theme.BizCardTheme
@@ -40,7 +50,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CreateBizCard(){
     Surface(modifier = Modifier.fillMaxWidth().fillMaxHeight()){
-        Card(modifier = Modifier.width(200.dp)
+        Card(modifier = Modifier
+            .width(200.dp)
             .height(390.dp)
             .padding(vertical = 60.dp, horizontal = 12.dp),
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
@@ -49,8 +60,35 @@ fun CreateBizCard(){
                 containerColor = Color.White
             )
         ){
-
+            Column(modifier = Modifier
+                .height(300.dp)
+                .fillMaxWidth(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally){
+                CreateImageProfile()
+                HorizontalDivider()
+            }
         }
+    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        shadowElevation = 4.dp,
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "profile picture",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop,
+        )
     }
 }
 
